d5f726cfe881f308e500503fe9c7a661fbbf40d8
Jeni Hoyt <ziahrahl@gmail.com>
2022-06-02 15:26:48 -0400

    create factory function for players

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
+// Module for gameboard
 const gameboard = (() => {
-    const gameboardArray = [0, 0, 0, 0, 0, 0, 0, 0];
+    const gameboardArray = ["X", "O", "X", "~", "X", "O", "~", "~", "O"];
+    const cells = document.querySelectorAll('.cell')
     const printBoard = () => {
-        for (let space of gameboardArray) {
-            console.log(space)
-        }
-
+        let i = 0
+        cells.forEach(cell => {
+            console.log(cell.id)
+            cell.innerText = gameboardArray[i];
+            i++;
+        })
     }
     return { printBoard }
 })();
 
 
-gameboard.printBoard()
+// gameboard.printBoard()
+
+
+// Factory Function for players
+const Player = (name) => {
+    const getName = () => name;
+}
+
+
+// object for flow of game
