19412f264ead5daeea7f8c6868345f4fcac9a27e
Jeni Hoyt <ziahrahl@gmail.com>
2022-06-10 16:07:20 -0400

    add function to create Player objects

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 // Button for Starting Game
 const newGame = document.querySelector('.new-game')
 
+// Factory Function for players
+const Player = (name, symbol) => {
+    const getName = () => name;
+    const getSymbol = () => symbol;
+    const makeMove = () => {
+
+    }
+    return { getName, getSymbol, makeMove }
+}
+
+
 // Module for gameboard
 const gameboard = (() => {
-    const gameboardArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8"];
-    const cells = document.querySelectorAll('.cell')
+    const gameboardArray = ["", "", "", "", "", "", "", "", ""];
+    const cells = document.querySelectorAll('.cell');
     const printBoard = () => {
         let i = 0
         cells.forEach(cell => {
@@ -13,89 +24,85 @@ const gameboard = (() => {
             i++;
         })
     }
+    printBoard();
     return { printBoard, gameboardArray }
 })();
 
 
-gameboard.printBoard()
-
-
-// Factory Function for players
-const Player = (name, symbol) => {
-    const getName = () => name;
-    const getSymbol = () => symbol;
-    const selectCell = () => {
-
-    }
-    return { getName, getSymbol, selectCell }
-}
-
 
 // object for flow of game
 const playGame = (() => {
-    
+    const p1NameUI = document.querySelector('.player-1-UI');
+    const p2NameUI = document.querySelector('.player-2-UI');
+    const namePrompt = document.querySelector('.prompt');
+
     // get player Names & assign symbol
     const getPlayerNames = () => {
-        const p1NameUI = document.querySelector('.player-1-UI')
-        const p2NameUI = document.querySelector('.player-2-UI')
-        const namePrompt = document.querySelector('.prompt')
         namePrompt.classList.remove('hide')
         const okBtn = document.querySelector('.ok-btn')
-        okBtn.addEventListener('click', () => {
-            const p1Name = document.querySelector('#p1-name').value;
-            const p2Name = document.querySelector('#p2-name').value;
-            p1NameUI.innerText = p1Name;
-            p2NameUI.innerText = p2Name;
-            namePrompt.classList.add('hide')
-                       
-        })
-        
-
+        okBtn.addEventListener('click', showNamesUI)
     }
-    return { getPlayerNames}
-    })();
-    // if they have, then continue
+    // display names in UI and create Players with factory function
+    const showNamesUI = () => {
+        const p1Name = document.querySelector('#p1-name').value;
+        const p2Name = document.querySelector('#p2-name').value;
+        p1NameUI.innerText = p1Name;
+        p2NameUI.innerText = p2Name;
+        namePrompt.classList.add('hide');
+        createPlayers(p1Name, p2Name);
+    }
 
-    // random selection of which player goes first
+    // create Player objects
+    const createPlayers = (p1Name, p2Name) => {
+        // console.log(p1Name)
+        const p1 = Player(p1Name, 'X');
+        const p2 = Player(p2Name, 'O');
+        // console.log(p2.getSymbol())
+        indicateCurrentPlayer(p2)
+        return p1, p2
+    }
 
-    // player turn
+    // indicate current player
+    const indicateCurrentPlayer = (player) => {
+        const currentPlayer = player.getName();
+        console.log(currentPlayer);
+        const controlsDiv = document.querySelector('.controls');
+        const currentPlayerDiv = document.createElement('div');
+        currentPlayerDiv.innerText = `${currentPlayer}'s turn`;
+        currentPlayerDiv.classList.add('current-player');
+        controlsDiv.appendChild(currentPlayerDiv)
+    }
 
 
-    // check for winner
+
+
+
+
+
+
+
+
+
+
+    return { getPlayerNames }
+})();
+
+
+
+
+
+
+// if they have, then continue
+
+// random selection of which player goes first
+
+// player turn
+
+
+// check for winner
 
 
 
 
 newGame.addEventListener('click', playGame.getPlayerNames)
 
-// if (p1Display.innerText === 'Player 1') {
-//     const p1NamePrompt = document.querySelector('.player1-name');
-//     p1NamePrompt.classList.remove('hide');
-//     const p1OkBtn = document.querySelector('#p1-ok');
-//     getPlayerNames(p10Btn, "1" )
-// const getPlayerNames = (okButton, playerNumber) => {
-    
-
-//         okButton.addEventListener('click', () => {
-//             const p1Name = document.querySelector(`#p${playerNumber}-name`).value;
-//             p1Display.innerText = p1Name;
-//             p1NamePrompt.classList.add('hide');
-//             const p1Symbol = 'X'
-//             const p1 = Player(p1Name, p1Symbol)
-//         });
-//     }
-//     if (p2Display.innerText === 'Player 2') {
-//         const p2NamePrompt = document.querySelector('.player2-name');
-//         p2NamePrompt.classList.remove('hide');
-//         const p2OkBtn = document.querySelector('#p2-ok');
-
-//         p2OkBtn.addEventListener('click', () => {
-//             const p2Name = document.querySelector('#p2-name').value;
-//             p2Display.innerText = p2Name;
-//             p2NamePrompt.classList.add('hide');
-//             const p2Symbol = 'O';
-//             const p2 = Player(p2Name, p2Symbol)
-//             console.log(p2.getName)
-//         });
-//     }
-//     return { getPlayerNames }
